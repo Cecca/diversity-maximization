d2a2262dd2deed7138342d192f3d8f6d88e79a4b
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-05-12 10:28:49 -0400

    Add StreamingState.addDelegate

--- src/test/scala/it/unipd/dei/diversity/StreamingStateTest.scala
+++ src/test/scala/it/unipd/dei/diversity/StreamingStateTest.scala
@@ -8,7 +8,7 @@ class StreamingStateTest extends FreeSpec with Matchers {
     "should accept the first k'+1 points" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingState(kPrime, 0, Distance.euclidean)
+      val sState = new StreamingState[Point](kPrime, 0, Distance.euclidean)
 
       sState.isInitializing should be (true)
 
@@ -22,7 +22,7 @@ class StreamingStateTest extends FreeSpec with Matchers {
     "after initialization, should reject the already seen points" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingState(kPrime, 0, Distance.euclidean)
+      val sState = new StreamingState[Point](kPrime, 0, Distance.euclidean)
 
       sState.isInitializing should be (true)
       points.foreach { p =>
@@ -39,7 +39,7 @@ class StreamingStateTest extends FreeSpec with Matchers {
     "after initialization, the threshold must be the minimum distance" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingState(kPrime, 0, Distance.euclidean)
+      val sState = new StreamingState[Point](kPrime, 0, Distance.euclidean)
 
       sState.isInitializing should be (true)
       points.foreach { p =>
@@ -47,15 +47,17 @@ class StreamingStateTest extends FreeSpec with Matchers {
       }
       sState.isInitializing should be (false)
 
-      sState.getThreshold should be (StreamingState.minDistance(
+      sState.getThreshold should be (StreamingState.minDistance[Point](
         sState.kernel, Distance.euclidean))
     }
   }
 
-  "The delegate merging" - {
-    "Should do nothing if there is no space for new delegates" in {
-      val sState = new StreamingState(2, 1, Distance.euclidean)
-      
+  "The delegate adding" - {
+    "Should add the delegate only if thehre is space" in {
+      val sState = new StreamingState[Point](2, 1, Distance.euclidean)
+      sState.addDelegate(0, Point(0)) should be (true)
+      sState.addDelegate(0, Point(1)) should be (false)
+      sState.addDelegate(1, Point(0)) should be (true)
     }
   }
 
