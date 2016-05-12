78e3da4c0dd8ca3c2c757c301eb75980b9686933
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-05-12 14:43:46 -0400

    Rename sState variables to coreset

--- src/test/scala/it/unipd/dei/diversity/StreamingCoresetChecks.scala
+++ src/test/scala/it/unipd/dei/diversity/StreamingCoresetChecks.scala
@@ -8,12 +8,12 @@ object StreamingCoresetChecks extends Properties("StreamingCoreset") {
   property("farness") =
     forAll(Gen.choose(2, 100), Gen.posNum[Int], Gen.choose(2, 1000))
     { (kernelSize, numDelegates, n) =>
-      val sState = new StreamingCoreset(kernelSize, numDelegates, Distance.euclidean)
+      val coreset = new StreamingCoreset(kernelSize, numDelegates, Distance.euclidean)
       (0 until n).foreach { _ =>
-        sState.update(Point.random(1))
+        coreset.update(Point.random(1))
       }
 
-      sState.numKernelPoints == 1 || sState.threshold <= sState.minKernelDistance
+      coreset.numKernelPoints == 1 || coreset.threshold <= coreset.minKernelDistance
     }
 
 }
--- src/test/scala/it/unipd/dei/diversity/StreamingCoresetTest.scala
+++ src/test/scala/it/unipd/dei/diversity/StreamingCoresetTest.scala
@@ -8,30 +8,30 @@ class StreamingCoresetTest extends FreeSpec with Matchers {
     "should accept the first k'+1 points" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
+      val coreset = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
 
-      sState.initializing should be (true)
+      coreset.initializing should be (true)
 
       points.foreach { p =>
-        sState.update(p) should be (true)
+        coreset.update(p) should be (true)
       }
 
-      sState.initializing should be (false)
+      coreset.initializing should be (false)
     }
 
     "after initialization, should reject the already seen points" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
+      val coreset = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
 
-      sState.initializing should be (true)
+      coreset.initializing should be (true)
       points.foreach { p =>
-        sState.update(p) should be (true)
+        coreset.update(p) should be (true)
       }
-      sState.initializing should be (false)
+      coreset.initializing should be (false)
       points.zipWithIndex.foreach { case (p, idx) =>
         withClue(s"Stream index $idx:") {
-          sState.update(p) should be (false)
+          coreset.update(p) should be (false)
         }
       }
     }
@@ -39,41 +39,41 @@ class StreamingCoresetTest extends FreeSpec with Matchers {
     "after initialization, the threshold must be the minimum distance" in {
       val kPrime = 10
       val points = (0 to kPrime).view.map(_ => Point.random(4))
-      val sState = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
+      val coreset = new StreamingCoreset[Point](kPrime, 0, Distance.euclidean)
 
-      sState.initializing should be (true)
+      coreset.initializing should be (true)
       points.foreach { p =>
-        sState.update(p) should be (true)
+        coreset.update(p) should be (true)
       }
-      sState.initializing should be (false)
+      coreset.initializing should be (false)
 
-      sState.threshold should be (StreamingCoreset.minDistance[Point](
-        sState._kernel, Distance.euclidean))
+      coreset.threshold should be (StreamingCoreset.minDistance[Point](
+        coreset._kernel, Distance.euclidean))
     }
   }
 
   "The delegate adding" - {
     "Should add the delegate only if thehre is space" in {
-      val sState = new StreamingCoreset[Point](2, 1, Distance.euclidean)
-      sState.addDelegate(0, Point(0)) should be (true)
-      sState.addDelegate(0, Point(1)) should be (false)
-      sState.addDelegate(1, Point(3)) should be (true)
+      val coreset = new StreamingCoreset[Point](2, 1, Distance.euclidean)
+      coreset.addDelegate(0, Point(0)) should be (true)
+      coreset.addDelegate(0, Point(1)) should be (false)
+      coreset.addDelegate(1, Point(3)) should be (true)
     }
   }
 
   "The delegate merging" - {
     "Should add only the delegates there's space for" in {
-      val sState = new StreamingCoreset[Point](2, 3, Distance.euclidean)
-      sState._kernel(0) = Point(0)
-      sState.addDelegate(0, Point(1)) should be (true)
-      sState.addDelegate(0, Point(2)) should be (true)
-      sState._kernel(1) = Point(3)
-      sState.addDelegate(1, Point(4)) should be (true)
-      sState.addDelegate(1, Point(5)) should be (true)
-      sState.addDelegate(1, Point(6)) should be (true)
+      val coreset = new StreamingCoreset[Point](2, 3, Distance.euclidean)
+      coreset._kernel(0) = Point(0)
+      coreset.addDelegate(0, Point(1)) should be (true)
+      coreset.addDelegate(0, Point(2)) should be (true)
+      coreset._kernel(1) = Point(3)
+      coreset.addDelegate(1, Point(4)) should be (true)
+      coreset.addDelegate(1, Point(5)) should be (true)
+      coreset.addDelegate(1, Point(6)) should be (true)
 
-      sState.mergeDelegates(0, 1)
-      sState.delegatesOf(0).toList should be (List(Point(1), Point(2), Point(3)))
+      coreset.mergeDelegates(0, 1)
+      coreset.delegatesOf(0).toList should be (List(Point(1), Point(2), Point(3)))
     }
   }
 
