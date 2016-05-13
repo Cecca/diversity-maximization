07206f69d2637437692b0ebdad499e51627e4bae
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-05-13 10:11:37 -0400

    Add simpler implementation of FarthestPointHeuristic

--- core/src/main/scala/it/unipd/dei/diversity/FarthestPointHeuristic.scala
+++ core/src/main/scala/it/unipd/dei/diversity/FarthestPointHeuristic.scala
@@ -1,5 +1,6 @@
 package it.unipd.dei.diversity
 
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 
 object FarthestPointHeuristic {
@@ -28,6 +29,32 @@ object FarthestPointHeuristic {
         i += 1
       }
       result
+    }
+  }
+
+  /* Just for testing purposes: this is a more idiomatic,
+   * albeit slower, implementation of the algorithm
+   */
+  private[diversity]
+  def runSlow[T: ClassTag](points: Array[T],
+                           k:Int,
+                           distance: (T, T) => Double): Array[T] = {
+    if (points.length <= k) {
+      points
+    } else {
+      // Initialize with the first point of the input
+      val result = ArrayBuffer[T](points(0))
+      while(result.size < k) {
+        // For each point look for the closest center
+        val (farthest, dist) = points.map { p =>
+          val dist = result.map { kp =>
+            distance(p, kp)
+          }.min
+          (p, dist)
+        }.maxBy(_._2)
+        result.append(farthest)
+      }
+      result.toArray
     }
   }
 
--- core/src/test/scala/it/unipd/dei/diversity/FarthestPointHeuristicTest.scala
+++ core/src/test/scala/it/unipd/dei/diversity/FarthestPointHeuristicTest.scala
@@ -1,20 +1,32 @@
 package it.unipd.dei.diversity
 
 import org.scalacheck._
-import org.scalacheck.Prop.{forAll, BooleanOperators}
+import org.scalacheck.Prop.{forAll, BooleanOperators, all}
 import Utils._
 
 object FarthestPointHeuristicTest extends Properties("FarthestPointHeuristic") {
 
-  property("anticover") = forAll { (pts: Array[Double], k: Int) =>
-    (pts.length >= 2 && k >= 2) ==> {
-      val points = pts.map(p => Point(p))
-      val result = FarthestPointHeuristic.run(points, k, Distance.euclidean)
-      val farness = minDistance(result, Distance.euclidean)
-      val radius = maxMinDistance(result, points, Distance.euclidean)
-      radius <= farness
+  property("anticover") =
+    forAll(Gen.listOf[Double](Gen.choose[Double](0.0, 1.0)), Gen.choose[Int](2, 100))
+    { (pts: List[Double], k: Int) =>
+      (pts.length >= 2 && k < pts.size) ==> {
+        val points = pts.map(p => Point(p)).toArray
+        val result = FarthestPointHeuristic.run(points, k, Distance.euclidean)
+        val farness = minDistance(result, Distance.euclidean)
+        val radius = maxMinDistance(result, points, Distance.euclidean)
+        radius <= farness
+      }
     }
 
-  }
+  property("simpler implementation") =
+    forAll(Gen.listOf[Double](Gen.choose[Double](0.0, 1.0)), Gen.choose[Int](2, 100))
+    { (pts: List[Double], k: Int) =>
+      (pts.size >= 2 && k < pts.size) ==> {
+        val points = pts.map(p => Point(p)).toArray
+        val actual = FarthestPointHeuristic.run(points, k, Distance.euclidean).toSet
+        val expected = FarthestPointHeuristic.runSlow(points, k, Distance.euclidean).toSet
+        s"$actual != $expected" |:(actual == expected)
+      }
+    }
 
 }
