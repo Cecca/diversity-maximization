33316f0dc9dd6549c5741a9c6ae189692aae0d2d
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-05-13 11:08:03 -0400

    Add the tree diversity

--- core/src/main/scala/it/unipd/dei/diversity/Diversity.scala
+++ core/src/main/scala/it/unipd/dei/diversity/Diversity.scala
@@ -1,5 +1,6 @@
 package it.unipd.dei.diversity
 
+import scala.collection.mutable
 import scala.reflect.ClassTag
 
 object Diversity {
@@ -37,6 +38,36 @@ object Diversity {
                           distance: (T, T) => Double): Double = {
     val kSubset = MatchingHeuristic.run(points, k, distance)
     clique(kSubset, distance)
+  }
+
+  def tree[T: ClassTag](points: IndexedSeq[T],
+                        distance: (T, T) => Double): Double = {
+    // Find a minimum spanning tree
+    val result = mutable.Set[T](points.head)
+    val candidates = mutable.Set[T](points.tail :_*)
+    val weights = mutable.ArrayBuffer[Double]()
+    while (candidates.nonEmpty) {
+      // Find the closest point
+      val (a, b, dist) = {
+        for {
+          p1 <- result
+          p2 <- candidates
+        } yield (p1, p2, distance(p1, p2))
+      }.view.minBy(_._3)
+      result.add(a)
+      result.add(b)
+      candidates.remove(a)
+      candidates.remove(b)
+      weights.append(dist)
+    }
+    weights.sum
+  }
+
+  def tree[T: ClassTag](points: IndexedSeq[T],
+                        k: Int,
+                        distance: (T, T) => Double): Double = {
+    val kSubset = FarthestPointHeuristic.run(points, k, distance)
+    tree(kSubset, distance)
   }
 
 }
