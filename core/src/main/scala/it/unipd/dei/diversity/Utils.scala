6f5fc107c2cd0dac7700ccb9fb62eb42c325028f
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-05-14 08:37:15 -0400

    Reimplement minDistance using the pairs iterator

--- core/src/main/scala/it/unipd/dei/diversity/Utils.scala
+++ core/src/main/scala/it/unipd/dei/diversity/Utils.scala
@@ -13,15 +13,7 @@ object Utils {
   def minDistance[T](points: IndexedSeq[T],
                      distance: (T, T) => Double): Double = {
     require(points.length >= 2, "At least two points are needed")
-    points.flatMap { p1 =>
-      points.flatMap { p2 =>
-        if (p1 != p2) {
-          Iterable(distance(p1, p2))
-        } else {
-          Iterable.empty
-        }
-      }
-    }.min
+    pairs(points).map{case (a, b) => distance(a,b)}.min
   }
 
   def maxMinDistance[T](pointsA: IndexedSeq[T],
