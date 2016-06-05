77646cffd2890988ac416105f563500f6d139b69
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-05 17:40:08 -0400

    Add object to read sequence files of points

---
+++ experiments/src/main/scala/it/unipd/dei/diversity/SerializationUtils.scala
@@ -0,0 +1,71 @@
+package it.unipd.dei.diversity
+
+import java.io.{ByteArrayInputStream, ObjectInputStream}
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.SequenceFile.Reader
+import org.apache.hadoop.io.{BytesWritable, NullWritable}
+
+object SerializationUtils {
+
+  /** Deserialize an object using Java serialization */
+  def deserialize[T](bytes: Array[Byte]): T = {
+    val bis = new ByteArrayInputStream(bytes)
+    val ois = new ObjectInputStream(bis)
+    ois.readObject.asInstanceOf[T]
+  }
+
+  def sequenceFile(file: String): Iterator[Point] = {
+    val path = new Path(file)
+    val conf = new Configuration()
+    if (path.getFileSystem(conf).isFile(path)) {
+      singleSequenceFile(path, conf)
+    } else {
+      multiSequenceFile(path, conf)
+    }
+  }
+
+  private def singleSequenceFile(path: Path, conf: Configuration): Iterator[Point] = {
+    val reader = new Reader(
+      conf,
+      Reader.file(path))
+    require(reader.getKeyClass.equals(classOf[NullWritable]),
+      s"Key class should be ${classOf[NullWritable]}")
+    require(reader.getValueClass.equals(classOf[BytesWritable]),
+      s"Value class should be ${classOf[BytesWritable]}")
+
+    val blocks = new Iterator[Array[Point]] {
+      var key = NullWritable.get()
+      var nextValue = new BytesWritable()
+      var value = new BytesWritable()
+      var _hasNext = true
+
+      // initialize the iterator
+      _hasNext = reader.next(key, nextValue)
+
+      override def hasNext: Boolean = _hasNext
+
+      override def next(): Array[Point] = {
+        if(!_hasNext)
+          throw new NoSuchElementException("No next element")
+        value = nextValue
+        val toReturn = deserialize[Array[Point]](value.copyBytes())
+        _hasNext = reader.next(key, nextValue)
+        toReturn
+      }
+    }
+
+    // Flatten the blocks
+    blocks.flatMap(_.iterator)
+  }
+
+  private def multiSequenceFile(path: Path, conf: Configuration): Iterator[Point] = {
+    val fs = path.getFileSystem(conf)
+    require(fs.isDirectory(path))
+    fs.listStatus(path).iterator.filter(fs => fs.getPath.getName.startsWith("part-")).flatMap { fileStatus =>
+      val file = fileStatus.getPath
+      singleSequenceFile(file, conf)
+    }
+  }
+}
