4f766ad8dcbb39bce4b745b13b0beb29525355fe
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-07 17:20:14 -0400

    Add Algorithm.random and Algorithm.sequential

--- experiments/src/main/scala/it/unipd/dei/diversity/Algorithm.scala
+++ experiments/src/main/scala/it/unipd/dei/diversity/Algorithm.scala
@@ -6,7 +6,9 @@ import it.unipd.dei.diversity.ExperimentUtil._
 import it.unipd.dei.experiment.Experiment
 import org.apache.spark.rdd.RDD
 
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
+import scala.util.Random
 
 object Algorithm {
 
@@ -44,6 +46,7 @@ object Algorithm {
                             distance: (T, T) => Double,
                             experiment: Experiment): MapReduceCoreset[T] = {
     require(kernelSize >= k)
+    experiment.tag("algorithm", "MapReduce")
 
     val parallelism = points.sparkContext.defaultParallelism
 
@@ -73,6 +76,41 @@ object Algorithm {
       ))
 
     coreset
+  }
+
+  /**
+    * Doesn't do much, just wraps the entire input in a coreset, just
+    * for uniformity with the rest.
+    */
+  def sequential[T:ClassTag](points: Vector[T],
+                             experiment: Experiment): Coreset[T] = {
+    experiment.tag("algorithm", "Sequential")
+    new Coreset[T] {
+      override def kernel: Vector[T] = points
+      override def delegates: Vector[T] = Vector.empty
+    }
+  }
+
+  def random[T:ClassTag](input: Iterable[T],
+                         k: Int,
+                         sampleProb: Double,
+                         distance: (T, T) => Double,
+                         experiment: Experiment): Coreset[T] = {
+    experiment.tag("algorithm", "Random")
+    println("Run!!")
+    val sample = ArrayBuffer[T]()
+    for (p <- input) {
+      if (Random.nextDouble() <= sampleProb) {
+        sample.append(p)
+      }
+    }
+    val randomSubset = Random.shuffle(sample).take(k).toVector
+    require(randomSubset.size == k)
+
+    new Coreset[T] {
+      override def kernel: Vector[T] = randomSubset
+      override def delegates: Vector[T] = Vector.empty
+    }
   }
 
 
