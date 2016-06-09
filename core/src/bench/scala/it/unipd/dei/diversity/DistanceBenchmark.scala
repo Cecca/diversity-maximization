bbc2b8b322d360b7e9da573bee6b1715050e7329
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-08 22:45:13 -0400

    Turn BagOfWords trait into a pure interface
    Moreover, provide a basic map-based implementation

--- core/src/bench/scala/it/unipd/dei/diversity/DistanceBenchmark.scala
+++ core/src/bench/scala/it/unipd/dei/diversity/DistanceBenchmark.scala
@@ -1,10 +1,8 @@
 package it.unipd.dei.diversity
 
-import org.roaringbitmap.RoaringBitmap
 import org.scalameter.api._
 
-import scala.collection.{BitSet, mutable}
-import scala.collection.immutable.HashSet
+import scala.collection.mutable
 import scala.util.Random
 
 object DistanceBenchmark extends Bench.OfflineReport {
@@ -12,55 +10,22 @@ object DistanceBenchmark extends Bench.OfflineReport {
   val vocabulary: Vector[Int] = (0 until 10000).toVector
 
   val sizes = Gen.exponential("size")(8, 8192, 2)
-  val simplePairs = for {
+  val mapPairs = for {
     size <- sizes
-  } yield (SimpleBOW(vocabulary, size), SimpleBOW(vocabulary, size))
-
-  val hashSetPairs = for {
-    size <- sizes
-  } yield (HashSetBOW(vocabulary, size), HashSetBOW(vocabulary, size))
-
-  val bitsetPairs = for {
-    size <- sizes
-  } yield (BitSetBOW(vocabulary, size), BitSetBOW(vocabulary, size))
-
+  } yield (BOWBuilders.mapBOW(vocabulary, size), BOWBuilders.mapBOW(vocabulary, size))
   val roaringPairs = for {
     size <- sizes
-  } yield (RoaringBOW(vocabulary, size), RoaringBOW(vocabulary, size))
-
-  val intPairs = for {
-    size <- sizes
-  } yield (IntBOW(vocabulary, size), IntBOW(vocabulary, size))
-
+  } yield (BOWBuilders.roaringBOW(vocabulary, size), BOWBuilders.roaringBOW(vocabulary, size))
 
   performance of "Distance.euclidean" in {
 
-    measure method "SimpleBOW" in {
-      using(simplePairs) in { case (a, b) =>
+    measure method "MapBOW" in {
+      using(mapPairs) in { case (a, b) =>
         Distance.euclidean(a, b)
       }
     }
-
-    measure method "HashSetBOW" in {
-      using(hashSetPairs) in { case (a, b) =>
-        Distance.euclidean(a, b)
-      }
-    }
-
-    measure method "BitSetBOW" in {
-      using(bitsetPairs) in { case (a, b) =>
-        Distance.euclidean(a, b)
-      }
-    }
-
     measure method "RoaringBOW" in {
-      using(roaringPairs) in { case (a, b) =>
-        Distance.euclidean(a, b)
-      }
-    }
-
-    measure method "IntBOW" in {
-      using(intPairs) in { case (a, b) =>
+      using(mapPairs) in { case (a, b) =>
         Distance.euclidean(a, b)
       }
     }
@@ -69,78 +34,19 @@ object DistanceBenchmark extends Bench.OfflineReport {
 
 }
 
-class SimpleBOW(override val wordCounts: mutable.HashMap[Int, Int]) extends BagOfWords[Int]
-object SimpleBOW {
-  def apply(vocabulary: Vector[Int], size: Int): SimpleBOW ={
-    val wc = mutable.HashMap(
+object BOWBuilders {
+
+  def randomTable(vocabulary: Vector[Int], size: Int) =
+    mutable.HashMap(
       Random.shuffle(vocabulary).take(size).map { w =>
         (w, Random.nextInt())
       } :_*)
-    new SimpleBOW(wc)
-  }
-}
 
-class HashSetBOW(override val wordCounts: mutable.HashMap[Int, Int]) extends BagOfWords[Int]
-object HashSetBOW {
-  def apply(vocabulary: Vector[Int], size: Int): HashSetBOW ={
-    val wc = mutable.HashMap(
-      Random.shuffle(vocabulary).take(size).map { w =>
-        (w, Random.nextInt())
-      } :_*)
-    new HashSetBOW(wc)
-  }
-}
+  def mapBOW(vocabulary: Vector[Int], size: Int): BagOfWords[Int] =
+    new MapBagOfWords[Int](randomTable(vocabulary, size))
 
-class BitSetBOW(override val wordCounts: mutable.HashMap[Int, Int]) extends BagOfWords[Int] {
-  val words: BitSet = BitSet(wordCounts.keys.toSeq :_*)
-  override def wordUnion(other: BagOfWords[Int]): Iterator[Int] = {
-    val union = this.words.intersect(other.asInstanceOf[BitSetBOW].words)
-    union.iterator
-  }
-}
-object BitSetBOW {
-  def apply(vocabulary: Vector[Int], size: Int): BitSetBOW ={
-    val wc = mutable.HashMap(
-      Random.shuffle(vocabulary).take(size).map { w =>
-        (w, Random.nextInt())
-      } :_*)
-    new BitSetBOW(wc)
-  }
-}
+  def roaringBOW(vocabulary: Vector[Int], size: Int): BagOfWords[Int] =
+    new IntBagOfWords(randomTable(vocabulary, size))
 
-class RoaringBOW(override val wordCounts: mutable.HashMap[Int, Int]) extends BagOfWords[Int]{
-
-  private val roaringWords = RoaringBitmap.bitmapOf(wordCounts.keys.toSeq :_*)
-
-  override def wordUnion(other: BagOfWords[Int]): Iterator[Int] = {
-    val otherRoaringWords = other.asInstanceOf[RoaringBOW].roaringWords
-    val union = RoaringBitmap.or(this.roaringWords, otherRoaringWords)
-    new Iterator[Int] {
-      val it = union.getIntIterator
-      override def hasNext: Boolean = it.hasNext
-      override def next(): Int = it.next()
-    }
-  }
-}
-object RoaringBOW {
-  def apply(vocabulary: Vector[Int], size: Int): RoaringBOW ={
-    val wc = mutable.HashMap(
-      Random.shuffle(vocabulary).take(size).map { w =>
-        (w, Random.nextInt())
-      } :_*)
-    new RoaringBOW(wc)
-  }
-}
-
-object IntBOW {
-  def apply(vocabulary: Vector[Int], size: Int): IntBagOfWords ={
-    val wc = mutable.HashMap(
-      Random.shuffle(vocabulary).take(size).map { w =>
-        (w, Random.nextInt())
-      } :_*)
-    new IntBagOfWords {
-      override def wordCounts: mutable.HashMap[Int, Int] = wc
-    }
-  }
 
 }
--- core/src/main/scala/it/unipd/dei/diversity/BagOfWords.scala
+++ core/src/main/scala/it/unipd/dei/diversity/BagOfWords.scala
@@ -5,21 +5,37 @@ import scala.collection.mutable
 
 trait BagOfWords[T] {
 
-  def wordCounts: mutable.HashMap[T, Int]
+  def words: Iterator[T]
 
-  def wordUnion(other: BagOfWords[T]): Iterator[T] =
-    this.wordCounts.keySet.union(other.wordCounts.keySet).iterator
+  def wordUnion(other: BagOfWords[T]): Iterator[T]
 
-  def wordIntersection(other: BagOfWords[T]): Iterator[T] =
-    this.wordCounts.keySet.intersect(other.wordCounts.keySet).iterator
+  def wordIntersection(other: BagOfWords[T]): Iterator[T]
 
-  def apply(word: T): Int = wordCounts.getOrElse(word, 0)
+  def apply(word: T): Int
 
 }
 
-trait IntBagOfWords extends BagOfWords[Int] {
+class MapBagOfWords[T](val wordCounts: mutable.HashMap[T, Int]) extends BagOfWords[T] {
+
+  override def words: Iterator[T] = wordCounts.keysIterator
+
+  override def wordUnion(other: BagOfWords[T]): Iterator[T] =
+    this.wordCounts.keySet.union(other.asInstanceOf[MapBagOfWords[T]].wordCounts.keySet).iterator
+
+  override def wordIntersection(other: BagOfWords[T]): Iterator[T] =
+    this.wordCounts.keySet.intersect(other.asInstanceOf[MapBagOfWords[T]].wordCounts.keySet).iterator
+
+  override def apply(word: T): Int = wordCounts.getOrElse(word, 0)
+
+}
+
+class IntBagOfWords(val wordCounts: mutable.HashMap[Int, Int]) extends BagOfWords[Int] {
 
   private val roaringWords = RoaringBitmap.bitmapOf(wordCounts.keys.toSeq :_*)
+
+  override def apply(word: Int): Int = wordCounts.getOrElse(word, 0)
+
+  override def words: Iterator[Int] = wordCounts.keysIterator
 
   override def wordUnion(o: BagOfWords[Int]): Iterator[Int] = o match {
     case other: IntBagOfWords =>
