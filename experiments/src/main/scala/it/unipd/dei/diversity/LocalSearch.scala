8f30034049f9148f55be1f751bdb33c76cf7e261
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-10 15:59:45 -0400

    Start working on LocalSearch

---
+++ experiments/src/main/scala/it/unipd/dei/diversity/LocalSearch.scala
@@ -0,0 +1,65 @@
+package it.unipd.dei.diversity
+
+import scala.reflect.ClassTag
+
+/**
+  * The local search algorithm of [AghamolaeiFZ15] and [IndykMMM14]
+  *
+  * [IndykMMM14]
+  * - Indyk, P., Mahabadi, S., Mahdian, M., & Mirrokni,
+  *   V. S. (2014). Composable core-sets for diversity and coverage
+  *   maximization. In , Proceedings of the 33rd ACM SIGMOD-SIGACT-SIGART
+  *   Symposium on Principles of Database Systems (pp. 100–108). New York,
+  *   NY, USA: ACM.
+  *
+  * [AghamolaeiFZ15]
+  * - Aghamolaei Sepideh, Majid Farhadi, and Hamid Zarrabi-Zadeh.
+  *   "Diversity Maximization via Composable Coresets."
+  */
+object LocalSearch {
+
+  def initialSet[T:ClassTag](input: IndexedSeq[T],
+                             k: Int,
+                             distance: (T, T) => Double): IndexedSeq[T] = {
+    if (input.length <= k) {
+      input
+    } else {
+      val result = Array.ofDim[T](k)
+      // Find the pair of points with maximum distance
+      var maxDist = 0.0
+      var a = 0
+      var b = 0
+      var i = 0
+      while (i<input.length) {
+        var j = i+1
+        while (j<input.length) {
+          val d = distance(input(i), input(j))
+          if (d > maxDist) {
+            a = i
+            b = j
+            maxDist = d
+          }
+          j += 1
+        }
+        i += 1
+      }
+      // Add them to the result
+      result(0) = input(a)
+      result(1) = input(b)
+
+      // Fill the point set with the other points
+      var h = 0
+      var rIdx = 2
+      while (h<input.length) {
+        if (h != a && h != b) {
+          result(rIdx) = input(h)
+          rIdx += 1
+        }
+        h += 1
+      }
+
+      result
+    }
+  }
+
+}
