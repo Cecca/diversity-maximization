2abf633226a6670a379aa2d4161ee7d4f31bf7a0
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-20 16:19:07 -0400

    Add main for Wikipedia experiment

---
+++ experiments/src/main/scala/it/unipd/dei/diversity/wiki/MainWiki.scala
@@ -0,0 +1,91 @@
+package it.unipd.dei.diversity.wiki
+
+import it.unipd.dei.diversity.{Algorithm, Approximation, SerializationUtils, _}
+import it.unipd.dei.experiment.Experiment
+import org.apache.spark.{SparkConf, SparkContext}
+import org.rogach.scallop.ScallopConf
+
+object MainWiki {
+
+  def main(args: Array[String]) {
+
+    // Read command line options
+    val opts = new Conf(args)
+    opts.verify()
+    val dataset = opts.dataset()
+    val kList = opts.delegates().split(",").map{_.toInt}
+    val kernelSizeList = opts.kernelSize().split(",").map{_.toInt}
+    val runs = opts.runs()
+    val approxRuns = opts.approxRuns()
+    val computeFarthest = opts.farthest()
+    val computeMatching = opts.matching()
+
+    val distance: (WikiBagOfWords, WikiBagOfWords) => Double = Distance.euclidean[String]
+
+    val sparkConfig = new SparkConf(loadDefaults = true)
+      .setAppName("Wikipedia MapReduce coresets")
+    val sc = new SparkContext(sparkConfig)
+
+    for {
+      r        <- 0 until runs
+      k        <- kList
+      kernSize <- kernelSizeList
+    } {
+      println(
+        s"""
+           |Experiment on $dataset
+           |  k  = $k
+           |  k' = $kernSize
+        """.stripMargin)
+      val experiment = new Experiment()
+        .tag("experiment", "Wikipedia")
+        .tag("version", BuildInfo.version)
+        .tag("git-revision", BuildInfo.gitRevision)
+        .tag("git-revcount", BuildInfo.gitRevCount)
+        .tag("git-branch", BuildInfo.gitBranch)
+        .tag("k", k)
+        .tag("kernel-size", kernSize)
+        .tag("computeFarthest", computeFarthest)
+        .tag("computeMatching", computeMatching)
+
+      val parallelism = sc.defaultParallelism
+      experiment.tag("parallelism", parallelism)
+      val documents = sc.textFile(dataset).map(WikiBagOfWords.fromLine)
+      val coreset: Coreset[WikiBagOfWords] =
+        Algorithm.mapReduce(documents, kernSize, k, distance, experiment)
+
+      Approximation.approximate(
+        coreset, k, distance, computeFarthest, computeMatching, approxRuns, experiment)
+
+      experiment.saveAsJsonFile()
+      println(experiment.toSimpleString)
+    }
+
+  }
+
+  class Conf(args: Array[String]) extends ScallopConf(args) {
+
+    lazy val dataset = opt[String](required = true)
+
+    lazy val delegates = opt[String](required = true)
+
+    lazy val kernelSize = opt[String](required = true)
+
+    lazy val runs = opt[Int](default = Some(1))
+
+    lazy val approxRuns = opt[Int](default = Some(1))
+
+    lazy val farthest = toggle(
+      default=Some(true),
+      descrYes = "Compute metrics based on the farthest-point heuristic",
+      descrNo  = "Don't compute metrics based on the farthest-point heuristic")
+
+    lazy val matching = toggle(
+      default=Some(true),
+      descrYes = "Compute metrics based on the matching heuristic",
+      descrNo  = "Don't compute metrics based on the matching heuristic")
+
+  }
+
+
+}
