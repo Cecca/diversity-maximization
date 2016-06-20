84602340aebb188246dbfef0b8a6aad7258bd84f
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-06-20 15:43:30 -0400

    Make WikiBagOfWords implement BagOfWords

--- experiments/src/main/scala/it/unipd/dei/diversity/wiki/WikiBagOfWords.scala
+++ experiments/src/main/scala/it/unipd/dei/diversity/wiki/WikiBagOfWords.scala
@@ -1,16 +1,28 @@
 package it.unipd.dei.diversity.wiki
 
-import it.unipd.dei.diversity.PorterStemmer
+import it.unipd.dei.diversity.{BagOfWords, Distance, Diversity, PorterStemmer}
 
 import scala.io.Source
 
 class WikiBagOfWords(val title: String,
                      val categories: Set[String],
-                     val words: Array[String],
-                     val counts: Array[Int]) {
+                     val wordsArray: Array[String],
+                     val countsArray: Array[Int])
+extends BagOfWords[String] with Serializable {
+
+  override def words: Iterator[String] = wordsArray.iterator
+
+  override def apply(word: String): Int = {
+    val idx = wordsArray.indexOf(word)
+    if (idx >= 0) {
+      countsArray(idx)
+    } else {
+      0
+    }
+  }
 
   override def toString: String =
-    s"$title: ($categories)\n${words.zip(counts).mkString(", ")}"
+    s"$title: ($categories)\n${wordsArray.zip(countsArray).mkString(", ")}"
 
 }
 
@@ -48,11 +60,12 @@ object WikiBagOfWords {
   }
 
   def main(args: Array[String]) {
-    val text = Source.fromFile(args(0))
-    for (line <- text.getLines()) {
-      println(WikiBagOfWords.fromLine(line))
-    }
+    val text =
+      if (args.length > 0) Source.fromFile(args(0))
+      else Source.fromInputStream(System.in)
+    val bows = text.getLines().map(s => WikiBagOfWords.fromLine(s)).toVector
     text.close()
+    println(Diversity.clique(bows, Distance.euclidean[String]))
   }
 
 }
