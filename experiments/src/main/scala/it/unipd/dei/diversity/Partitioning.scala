96ad079332dee5d2842d19594cfefe8258d300d5
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-10-05 15:27:56 +0200

    Refactor partitioning code

--- experiments/src/main/scala/it/unipd/dei/diversity/Partitioning.scala
+++ experiments/src/main/scala/it/unipd/dei/diversity/Partitioning.scala
@@ -16,14 +16,14 @@
 
 package it.unipd.dei.diversity
 
-import org.apache.spark.HashPartitioner
-import org.apache.spark.rdd.RDD
+import scala.reflect.ClassTag
+import scala.util.Random
+
 import it.unipd.dei.diversity.ExperimentUtil._
 import it.unipd.dei.experiment.Experiment
+import org.apache.spark.{HashPartitioner, SparkContext}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
-import scala.reflect.ClassTag
-
-import scala.util.Random
 
 /**
   * Various partitioning strategies. Some strategies can be
@@ -31,47 +31,14 @@ import scala.util.Random
   */
 object Partitioning {
 
-  def random(rdd: RDD[Point], experiment: Experiment): RDD[Array[Point]] = {
-    val (result, time): (RDD[Array[Point]], Long) = timed {
-      val parallelism = rdd.sparkContext.defaultParallelism
-      val _res =
-        if (rdd.getNumPartitions < parallelism) {
-          println("Increasing the number of partitions")
-          rdd.repartition(parallelism)
-        } else if (rdd.getNumPartitions > parallelism) {
-          println("Decreasing the number of partitions")
-          rdd.coalesce(parallelism)
-        } else {
-          rdd
-        }
-      // Force count to compute time
-      val _glom = _res.glom().persist(StorageLevel.MEMORY_AND_DISK)
-      _glom.count()
-      _glom
-    }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-    result
-  }
-
-  def shuffle[T:ClassTag](rdd: RDD[T], experiment: Experiment): RDD[Array[T]] = {
+  private def checkpointingPartitioner[T:ClassTag](rdd: RDD[T], experiment: Experiment)(fn: (RDD[T], Int) => RDD[Array[T]]) = {
     if (rdd.sparkContext.getCheckpointDir.isEmpty) {
       rdd.sparkContext.setCheckpointDir("/tmp")
       println(s"Checkpoint directory set to ${rdd.sparkContext.getCheckpointDir.get}")
     }
-    println("Shuffling the data!")
+    val parallelism = rdd.sparkContext.defaultParallelism
     val (result, time): (RDD[Array[T]], Long) = timed {
-      val parallelism = rdd.sparkContext.defaultParallelism
-      val _res = rdd.map { p =>
-        val pidx = Random.nextInt(parallelism)
-        (pidx, p)
-      }.partitionBy(new HashPartitioner(parallelism)).mapPartitions({ points =>
-        Iterator(points.map(_._2).toArray)
-      }, preservesPartitioning = true)
-        .persist(StorageLevel.MEMORY_ONLY)
+      val _res = fn(rdd, parallelism).persist(StorageLevel.MEMORY_ONLY)
 
       // Note: it would be nice to be able to call
       // _res.localCheckpoint(), since it is faster. However, it
@@ -96,134 +63,99 @@ object Partitioning {
     result
   }
 
+  private def byElementPartitioner[T:ClassTag](rdd: RDD[T], experiment: Experiment)(fn: (T, Int) => (Int, T)) = {
+    checkpointingPartitioner(rdd, experiment) { (rdd, parallelism) =>
+      rdd.map { p => fn(p, parallelism) }
+        .partitionBy(new HashPartitioner(parallelism))
+        .mapPartitions({ points =>
+          Iterator(points.map(_._2).toArray)
+        }, preservesPartitioning = true)
+    }
+  }
+
+  def random(rdd: RDD[Point], experiment: Experiment): RDD[Array[Point]] = {
+    checkpointingPartitioner(rdd, experiment) { (rdd, parallelism) =>
+      val _res = if (rdd.getNumPartitions < parallelism) {
+        println("Increasing the number of partitions")
+        rdd.repartition(parallelism)
+      } else if (rdd.getNumPartitions > parallelism) {
+        println("Decreasing the number of partitions")
+        rdd.coalesce(parallelism)
+      } else {
+        rdd
+      }
+      _res.glom()
+    }
+  }
+
+  def shuffle[T:ClassTag](rdd: RDD[T], experiment: Experiment): RDD[Array[T]] = {
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      val pidx = Random.nextInt(parallelism)
+      (pidx, p)
+    }
+  }
+
   def radius(rdd: RDD[Point],
              zero: Point,
              distance: (Point, Point) => Double,
              experiment: Experiment): RDD[Array[Point]] = {
-    val parallelism = rdd.sparkContext.defaultParallelism
-    val (result, time): (RDD[Array[Point]], Long) = timed {
-      val _res = rdd.map { p =>
-        val pidx = math.floor(distance(p, zero)*parallelism).toInt
-        (pidx, p)
-      }.partitionBy(new HashPartitioner(parallelism))
-        .mapPartitions({ points => Iterator(points.map(_._2).toArray) }, preservesPartitioning = true)
-        .persist(StorageLevel.MEMORY_AND_DISK)
-      // Force evaluation to get timing
-      _res.count()
-      _res
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      val pidx = math.floor(distance(p, zero)*parallelism).toInt
+      (pidx, p)
     }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-    result
   }
 
   def radiusOld(rdd: RDD[Point],
-                zero: Point,
-                distance: (Point, Point) => Double,
-                experiment: Experiment): RDD[Array[Point]] = {
-    val parallelism = rdd.sparkContext.defaultParallelism
-    val (result, time): (RDD[Array[Point]], Long) = timed {
-      val _res = rdd.map { p =>
-        val pidx = math.floor(distance(p, zero) / 0.8 * parallelism).toInt
-        (pidx, p)
-      }.partitionBy(new HashPartitioner(parallelism))
-        .mapPartitions({ points => Iterator(points.map(_._2).toArray) }, preservesPartitioning = true)
-        .persist(StorageLevel.MEMORY_AND_DISK)
-      // Force evaluation to get timing
-      _res.count()
-      _res
+             zero: Point,
+             distance: (Point, Point) => Double,
+             experiment: Experiment): RDD[Array[Point]] = {
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      val pidx = math.floor(distance(p, zero) / 0.8 * parallelism).toInt
+      (pidx, p)
     }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-    result
   }
 
-  def polar2D(rdd: RDD[Point], experiment: Experiment): RDD[Array[Point]] = {
-    val parallelism = rdd.sparkContext.defaultParallelism
-    val (res, time) = timed {
-      val _res = rdd.map { p =>
-        require(p.dimension >= 2)
-        val x = p(0)
-        val y = p(1)
-        val angle = math.atan2(y, x)
-        val shiftedAngle = if (angle >= 0) angle else angle + 2 * math.Pi
-        val pidx = math.floor(shiftedAngle / (2 * math.Pi) * parallelism).toInt
-        (pidx, p)
-      }.partitionBy(new HashPartitioner(parallelism)).mapPartitions(
-        { points => Iterator(points.map(_._2).toArray) },
-        preservesPartitioning = true).persist(StorageLevel.MEMORY_AND_DISK)
-      _res.count()
-      _res
+  def polar2D(rdd: RDD[Point],
+             experiment: Experiment): RDD[Array[Point]] = {
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      require(p.dimension >= 2)
+      val x = p(0)
+      val y = p(1)
+      val angle = math.atan2(y, x)
+      val shiftedAngle = if (angle >= 0) angle else angle + 2 * math.Pi
+      val pidx = math.floor(shiftedAngle / (2 * math.Pi) * parallelism).toInt
+      (pidx, p)
     }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-
-    res
   }
 
   def grid(rdd: RDD[Point], experiment: Experiment): RDD[Array[Point]] = {
-    val parallelism = rdd.sparkContext.defaultParallelism
-    val (res, time) = timed {
-      val _res = rdd.map { p =>
-        var index: Int = 0
-        var multiplier = 1
-        for (coord <- p.data) {
-          require(coord <= 1 && coord >= -1)
-          val j = ((coord + 1.0)*parallelism/2.0).toInt
-          require(0 <= j && j <= parallelism)
-          index += j*multiplier
-          multiplier *= parallelism
-        }
-        (index, p)
-      }.partitionBy(new HashPartitioner(parallelism)).mapPartitions(
-        { points => Iterator(points.map(_._2).toArray) },
-        preservesPartitioning = true).persist(StorageLevel.MEMORY_AND_DISK)
-      _res.count()
-      _res
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      var index: Int = 0
+      var multiplier = 1
+      for (coord <- p.data) {
+        require(coord <= 1 && coord >= -1)
+        val j = ((coord + 1.0)*parallelism/2.0).toInt
+        require(0 <= j && j <= parallelism)
+        index += j*multiplier
+        multiplier *= parallelism
+      }
+      (index, p)
     }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-    res
   }
 
   def unitGrid(rdd: RDD[Point], experiment: Experiment): RDD[Array[Point]] = {
-    val parallelism = rdd.sparkContext.defaultParallelism
-    val (res, time) = timed {
-      val _res = rdd.map { p =>
-        var index: Int = 0
-        var multiplier = 1
-        for (coord <- p.data) {
-          require(coord <= 1 && coord >= 0)
-          val j = (coord*parallelism).toInt
-          require(0 <= j && j <= parallelism)
-          index += j*multiplier
-          multiplier *= parallelism
-        }
-        (index, p)
-      }.partitionBy(new HashPartitioner(parallelism)).mapPartitions(
-        { points => Iterator(points.map(_._2).toArray) },
-        preservesPartitioning = true).persist(StorageLevel.MEMORY_AND_DISK)
-      _res.count()
-      _res
+    byElementPartitioner(rdd, experiment) { (p, parallelism) =>
+      var index: Int = 0
+      var multiplier = 1
+      for (coord <- p.data) {
+        require(coord <= 1 && coord >= 0)
+        val j = (coord*parallelism).toInt
+        require(0 <= j && j <= parallelism)
+        index += j*multiplier
+        multiplier *= parallelism
+      }
+      (index, p)
     }
-    experiment.append("times",
-      jMap(
-        "component" -> "partitioning",
-        "time"      -> convertDuration(time, reportTimeUnit)
-      ))
-    res
   }
 
 }
